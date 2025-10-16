78e0311d323e769fb88f87350cae5dfc8c41d158
sheenahamilton <sheenhamilton@gmail.com>
2025-10-15 21:12:21 -0600

    add config for authorization

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set('trust proxy', 1);
 // Session configuration
 app.use(
     session({
